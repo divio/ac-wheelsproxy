7732a1939b9fa74b660ab83dc25be38ea0b73f70
Jonathan Stoppani <jonathan@stoppani.name>
2015-10-05 15:27:53 +0200

    Add sentry support

--- README.rst
+++ README.rst
@@ -6,7 +6,7 @@ TODOs
 * Deployment
 * Logging + Sentry (incl removing print statements)
 * Add more admin functionalities (tasks to clear/rebuild packages, etc)
-* Move caching and storage config to coolfig
+* Move storage config to coolfig
 * README + Documentation
 * Add empty cache action to the admin (+ empty cache when adding/removing packages manually?)
 * Add rebuild/sync index actions to the admin
--- index/settings.py
+++ index/settings.py
@@ -1,8 +1,5 @@
-from coolfig import Settings, Value
-from coolfig.types import LazyCallable, boolean
-from coolfig.schema import DictValue
-
-django_cache_url = LazyCallable('environ', 'Env.cache_url_config')
+from coolfig import Settings, Value, Dictionary
+from coolfig.types import boolean
 
 
 class AppSettings(Settings):
@@ -10,11 +7,18 @@ class AppSettings(Settings):
     DOCKER_BUILDER_ENDPOINT = Value(str)
     DOCKER_BUILDER_CERTS = Value(str)
     TEMP_BUILD_ROOT = Value(str, default='/tmp')
-    CACHES = DictValue(django_cache_url, str.lower)
-    BUILDS_STORAGE_ACCESS_KEY = Value(str)
-    BUILDS_STORAGE_SECRET_KEY = Value(str)
-    BUILDS_STORAGE_BUCKET_NAME = Value(str)
+
     SECURE_SSL_REDIRECT = Value(boolean, default=False)
     SESSION_COOKIE_SECURE = Value(boolean, default=False)
     CSRF_COOKIE_SECURE = Value(boolean, default=False)
     SECURE_HSTS_SECONDS = Value(int, default=0)
+
+    RAVEN_CONFIG = Dictionary({
+        'DSN': Value(str, key='SENTRY_DSN', default=None),
+    })
+
+    # TODO: Move to coolfig as a single DictValue
+    # (similar to django_database_url and django_cache_url)
+    BUILDS_STORAGE_ACCESS_KEY = Value(str)
+    BUILDS_STORAGE_SECRET_KEY = Value(str)
+    BUILDS_STORAGE_BUCKET_NAME = Value(str)

6c8c5c3286860f7ffcc8e96fc8c3f4ff2b44f0b4
Jonathan Stoppani <jonathan@stoppani.name>
2015-10-10 23:21:14 +0200

    Make the image buildable

--- celery_app/settings.py
+++ celery_app/settings.py
@@ -5,7 +5,7 @@ class AppSettings(Settings):
     CELERY_IGNORE_RESULT = Value(types.boolean, default=False)
     CELERY_STORE_ERRORS_EVEN_IF_IGNORED = Value(types.boolean, default=True)
     BROKER_URL = Value(str, default=None)
-    CELERY_RESULT_BACKEND = Value(str, key='BROKER_URL')
+    CELERY_RESULT_BACKEND = Value(str, key='BROKER_URL', default=None)
     CELERY_TIMEZONE = Value(str, default='UTC')
     CELERY_ACCEPT_CONTENT = Value(types.list(str), default=['json'])
     CELERY_TASK_SERIALIZER = Value(str, default='json')

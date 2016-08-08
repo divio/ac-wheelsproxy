5cecfb3c5b4fe756c8b37a05df613ecbe6dd8f04
Jonathan Stoppani <jonathan@stoppani.name>
2016-08-09 01:57:07 +0200

    Support for devpi servers

---
+++ index/client.py
@@ -0,0 +1,225 @@
+import logging
+from collections import namedtuple
+import io
+import os
+
+import six
+
+import requests
+
+import furl
+
+from six.moves import xmlrpc_client, range
+
+from execnet.gateway_base import Unserializer
+
+
+log = logging.getLogger(__name__)
+
+
+class PackageNotFound(Exception):
+    pass
+
+
+class Release(namedtuple('Release', ['url', 'md5_digest', 'type'])):
+    @staticmethod
+    def guess_type(url):
+        if url.endswith('.whl'):
+            return 'bdist_wheel'
+        elif url.endswith('.tar.gz'):
+            return 'sdist'
+        else:
+            raise ValueError('Cannot guess package type of `{}`'.format(url))
+
+
+class IndexAPIClient(object):
+    def __init__(self, url):
+        self.url = url
+
+    def changelog_last_serial(self):
+        raise NotImplementedError
+
+    def list_packages(self):
+        raise NotImplementedError
+
+    def iter_updated_packages(self, since_serial):
+        raise NotImplementedError
+
+    def get_package_releases(self, package_name):
+        raise NotImplementedError
+
+    def get_version_releases(self, package_name, version):
+        raise NotImplementedError
+
+
+class PyPIClient(IndexAPIClient):
+    def __init__(self, model):
+        super(PyPIClient, self).__init__(model)
+        self.client = xmlrpc_client.ServerProxy(self.url)
+        self.session = requests.Session()
+
+    def changelog_last_serial(self):
+        return self.client.changelog_last_serial()
+
+    def list_packages(self):
+        return self.client.list_packages()
+
+    def iter_updated_packages(self, since_serial):
+        events = self.client.changelog_since_serial(since_serial)
+        while events:
+            seen_packages = set()
+            for event in events:
+                package_name, _, _, _, event_serial = event
+                if package_name in seen_packages:
+                    # This package was already seen once during this
+                    # loop, which means we have already updated it once.
+                    yield None, event_serial
+                else:
+                    seen_packages.add(package_name)
+                    yield package_name, event_serial
+            events = self.client.changelog_since_serial(event_serial)
+
+    def _clean_releases(self, version_details):
+        return [Release(
+            rel['url'],
+            rel['md5_digest'],
+            rel['packagetype'],
+        ) for rel in version_details]
+
+    def get_package_releases(self, package_name):
+        url = furl.furl(self.url)
+        url.path.add([package_name, 'json'])
+
+        response = self.session.get(url)
+        if response.status_code == 404:
+            raise PackageNotFound()
+        if response.status_code >= 300:
+            content = response.content
+            log.warning('Invalid response {} from index {} with content: {!r}'
+                        .format(response.status_code, self.url, content))
+            raise RuntimeError('Invalid response from index: {}'
+                               .format(response.status_code))
+
+        releases = response.json()['releases']
+        return {k: self._clean_releases(v) for k, v in six.iteritems(releases)}
+
+
+class DevPIClient(IndexAPIClient):
+    def __init__(self, model):
+        super(DevPIClient, self).__init__(model)
+        self.api_session = requests.Session()
+        self.api_session.headers.update({
+            'Accept': 'application/json',
+        })
+
+    def changelog_last_serial(self):
+        r = self.api_session.get(self.url)
+        return int(r.headers['x-devpi-serial']) - 1
+
+    def _iter_stage_packages(self, stage_url):
+        r = self.api_session.get(stage_url)
+        r.raise_for_status()
+        payload = r.json()
+
+        root_url = furl.furl(stage_url)
+        root_url.path.set(root_url.path.segments[:-2])
+
+        for base in payload['result']['bases']:
+            url = root_url.copy()
+            url.path.add(base.split('/'))
+            for project in self._iter_stage_packages(url):
+                yield project
+
+        for project in payload['result']['projects']:
+            yield project
+
+    def list_packages(self):
+        return list(set(self._iter_stage_packages(self.url)))
+
+    def _clean_releases(self, version_details):
+        return [Release(
+            rel['href'],
+            rel['md5'],
+            Release.guess_type(rel['href']),
+        ) for rel in version_details.get('+links', [])]
+
+    def get_package_releases(self, package_name):
+        url = furl.furl(self.url)
+        url.path.add(package_name)
+
+        response = self.api_session.get(url)
+        if response.status_code == 404:
+            raise PackageNotFound()
+        if response.status_code >= 300:
+            content = response.content
+            log.warning('Invalid response {} from index {} with content: {!r}'
+                        .format(response.status_code, url, content))
+            raise RuntimeError('Invalid response from index: {}'
+                               .format(response.status_code))
+
+        releases = response.json()['result']
+        return {k: self._clean_releases(v) for k, v in six.iteritems(releases)}
+
+    def _load_payload(self, payload):
+        payload = io.BytesIO(payload)
+        return (
+            Unserializer(payload, strconfig=(False, False))
+            .load(versioned=False)
+        )
+
+    def iter_updated_packages(self, since_serial):
+        changelog_url = furl.furl(self.url)
+        changelog_url.path.set(changelog_url.path.segments[:-2])
+        changelog_url.path.add('+changelog')
+
+        current_serial = self.changelog_last_serial()
+
+        while since_serial < current_serial:
+            seen_packages = set()
+            for event_serial in range(since_serial + 1, current_serial + 1):
+                url = changelog_url.copy()
+                url.path.add(str(event_serial))
+                response = self.api_session.get(url, timeout=5)
+                response.raise_for_status()
+
+                event = self._load_payload(response.content)[0]
+                for k, v in event.items():
+                    event_type, backserial, value = v
+                    method_name = 'handle_{}'.format(event_type.upper())
+                    package_name = getattr(self, method_name)(k, value)
+                    if not package_name or package_name in seen_packages:
+                        # The event did not contain any relevant information
+                        # or this package was already seen once during this
+                        # loop, which means we have already updated it once.
+                        yield None, event_serial
+                    else:
+                        seen_packages.add(package_name)
+                        yield package_name, event_serial
+            since_serial = current_serial
+            current_serial = self.changelog_last_serial()
+
+    def handle_USERLIST(self, key, payload):
+        pass  # Nothing to do here
+
+    def handle_USER(self, key, payload):
+        pass  # Nothing to do here
+
+    def handle_PROJNAMES(self, key, payload):
+        pass  # Nothing to do here
+
+    def handle_PROJVERSION(self, key, payload):
+        return key.split('/')[2]
+
+    def handle_PROJVERSIONS(self, key, payload):
+        return key.split('/')[2]
+
+    def handle_STAGEFILE(self, key, payload):
+        if payload:
+            # A new file was added
+            return payload.get('projectname')
+
+    def handle_PYPIFILE_NOMD5(self, key, payload):
+        pass  # Nothing to do here
+
+    def handle_PYPILINKS(self, key, payload):
+        pass  # Nothing to do here
--- index/models.py
+++ index/models.py
@@ -3,9 +3,6 @@ import logging
 import re
 
 import six
-from six.moves import xmlrpc_client
-import requests
-from yurl import URL
 from pkg_resources import parse_version
 
 from django.db import models
@@ -14,20 +11,25 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.utils.functional import cached_property
+from django.utils.module_loading import import_string
 from django.contrib.postgres.fields import JSONField
 
-from . import storage, tasks, builder, utils
+from extended_choices import Choices
+
+from . import storage, tasks, builder, utils, client
 
 
 log = logging.getLogger(__name__)
 
 
+INDEX_BACKENDS = Choices(
+    ('PYPI', 'index.client.PyPIClient', _('PyPI')),
+    ('DEVPI', 'index.client.DevPIClient', _('DevPI')),
+)
+
+
 def normalize_package_name(package_name):
     return re.sub(r'(\.|-|_)+', '-', package_name.lower())
-
-
-class PackageNotFound(Exception):
-    pass
 
 
 class Platform(models.Model):
@@ -52,88 +54,68 @@ class Platform(models.Model):
 
 class BackingIndex(models.Model):
     slug = models.SlugField(unique=True)
-    url = models.URLField()
+    url = models.URLField(verbose_name=_('URL'))
     last_update_serial = models.BigIntegerField(null=True, blank=True)
+    backend = models.CharField(
+        max_length=255,
+        choices=INDEX_BACKENDS,
+        default=INDEX_BACKENDS.PYPI,
+    )
+
+    class Meta:
+        verbose_name_plural = _('backing indexes')
 
     def __str__(self):
         return self.slug
 
     def get_client(self):
-        return xmlrpc_client.ServerProxy(self.url)
+        Client = import_string(self.backend)
+        return Client(self.url)
 
-    @cached_property
-    def client(self):
-        return self.get_client()
-
-    def get_package_details_url(self, package_name, version=None):
-        url = '{}/{}'.format(self.url.rstrip('/'), package_name)
-        if version:
-            url = '{}/{}'.format(url, version)
-        url = '{}/{}'.format(url, 'json')
-        return url
-
-    def get_package_details(self, package_name, version=None, session=None):
-        url = self.get_package_details_url(package_name, version)
-        if not session:
-            session = requests
-        response = session.get(url)
-        if response.status_code == 404:
-            raise PackageNotFound()
-        if response.status_code >= 300:
-            content = response.content
-            log.warning('Invalid response {} from index {} with content: {!r}'
-                        .format(response.status_code, self.url, content))
-            raise RuntimeError('Invalid response from index: {}'
-                               .format(response.status_code))
-        return response.json()
+    client = cached_property(get_client)
 
     def get_package(self, package_name):
         normalized_package_name = normalize_package_name(package_name)
         package, created = Package.objects.get_or_create(
-            index=self, slug=normalized_package_name,
-            defaults={'name': package_name})
+            index=self,
+            slug=normalized_package_name,
+            defaults={'name': package_name},
+        )
         return package
 
-    def last_upstream_serial(self):
-        return self.client.changelog_last_serial()
-
-    def _unsynced_events(self):
-        return self.client.changelog_since_serial(self.last_update_serial)
-
     def itersync(self):
-        session = requests.Session()
-        events = self._unsynced_events()
-        while events:
-            for event in events:
-                package_name, _, _, _, self.last_update_serial = event
-                if not self.import_package(package_name, session):
+        serial = self.last_update_serial
+        packages_to_update = self.client.iter_updated_packages(serial)
+        for package_name, serial in packages_to_update:
+            if package_name:
+                if not self.import_package(package_name):
                     # Nothing imported: remove the package
                     Package.objects.filter(
-                         index=self,
-                         slug=normalize_package_name(package_name),
+                        index=self,
+                        slug=normalize_package_name(package_name),
                     ).delete()
+            if serial > self.last_update_serial:
+                self.last_update_serial = serial
                 yield self.last_update_serial
-            events = self._unsynced_events()
         self.save(update_fields=['last_update_serial'])
 
     def sync(self):
         for i in self.itersync():
             pass
 
-    def import_package(self, package_name, session=None):
+    def import_package(self, package_name):
         # log.info('importing {} from {}'.format(package_name, self.url))
         try:
-            payload = self.get_package_details(package_name, session=session)
-        except PackageNotFound:
+            versions = self.client.get_package_releases(package_name)
+        except client.PackageNotFound:
             log.debug('package {} not found on {}'
                       .format(package_name, self.url))
             return
-        versions = payload['releases']
         if not versions:
             log.debug('no versions found for package {} on {}'
                       .format(package_name, self.url))
             return
-        package = self.get_package(payload['info']['name'])
+        package = self.get_package(package_name)
         release_ids = []
         for version, releases in six.iteritems(versions):
             release_details = package.get_best_release(releases)
@@ -179,29 +161,34 @@ class Package(models.Model):
 
     def get_best_release(self, releases):
         for release in releases:
-            if release['packagetype'] == 'sdist':
+            if release.type == 'sdist':
                 return release
         for release in releases:
-            if release['packagetype'] == 'bdist_wheel':
-                if release['filename'].endswith('-py2.py3-none-any.whl'):
+            if release.type == 'bdist_wheel':
+                if release.url.endswith('-py2.py3-none-any.whl'):
                     return release
 
-    def get_release(self, version, details=None):
-        release, created = Release.objects.get_or_create(
+    def get_release(self, version, release=None):
+        instance, created = Release.objects.get_or_create(
             package=self, version=version)
         if created:
-            if details:
-                release.original_details = details
-            else:
-                info = self.index.get_package_details(self.slug, version)
-                releases = info['releases'][version]
-                release.original_details = self.get_best_release(releases)
-            assert release.original_details
-            release.save()
-        elif details:
-            release.original_details = details
-            release.save()
-        return release
+            if not release:
+                releases = self.index.client.get_version_releases(
+                    self.slug,
+                    version,
+                )
+                release = self.get_best_release(releases)
+
+            instance.url = release.url
+            instance.md5_digest = release.md5_digest
+            assert instance.url
+            assert instance.md5_digest
+            instance.save(update_fields=['url', 'md5_digest'])
+        elif release:
+            instance.url = release.url
+            instance.md5_digest = release.md5_digest
+            instance.save(update_fields=['url', 'md5_digest'])
+        return instance
 
     @classmethod
     def get_cache_key(cls, namespace, index_slug, platform_slug, package_name):
@@ -250,7 +237,14 @@ class Package(models.Model):
 class Release(models.Model):
     package = models.ForeignKey(Package)
     version = models.CharField(max_length=200)
-    original_details = JSONField(null=True, blank=True)
+    url = models.URLField(blank=True, max_length=255, default='')
+    md5_digest = models.CharField(
+        verbose_name=_('MD5 digest'),
+        max_length=32,
+        default='',
+        blank=True,
+        editable=False,
+    )
     last_update = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -271,7 +265,7 @@ class Release(models.Model):
 
 
 def upload_build_to(self, filename):
-    return '{index}/{package}/{version}/{platform}/{filename}'.format(
+    return '{index}/{platform}/{package}/{version}/{filename}'.format(
         index=self.release.package.index.slug,
         package=self.release.package.slug,
         version=self.release.version,
@@ -354,10 +348,7 @@ class Build(models.Model):
 
     @property
     def original_url(self):
-        try:
-            return self.release.original_details['url']
-        except TypeError:
-            return ''
+        return self.release.url
 
     @property
     def requirements(self):
@@ -400,4 +391,4 @@ class Build(models.Model):
         if self.is_built():
             return self.md5_digest
         else:
-            return self.release.original_details['md5_digest']
+            return self.release.md5_digest

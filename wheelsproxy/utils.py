69edbe785a5649f8c5b214ccf86ce6ec726cb892
Jonathan Stoppani <jonathan@stoppani.name>
2016-08-24 15:09:40 +0200

    Return requirements unchanged for unresolvable specs
    E.g. local packages

--- wheelsproxy/utils.py
+++ wheelsproxy/utils.py
@@ -1,6 +1,7 @@
 import re
 
 from pkg_resources import Requirement
+from pkg_resources import yield_lines
 
 
 REQ_REGEX = re.compile(r'^(?P<name>[\S]+)(?: \((?P<version>[^\)]+)\))?$')
@@ -25,3 +26,26 @@ class UniquesIterator(object):
                 yield obj
 
     __call__ = not_seen_yet
+
+
+def split_requirements(strs):
+    """Yield ``Requirement`` objects for each specification in `strs`
+    `strs` must be a string, or a (possibly-nested) iterable thereof.
+    """
+    # create a steppable iterator, so we can handle \-continuations
+    lines = iter(yield_lines(strs))
+
+    for line in lines:
+        # Drop comments -- a hash without a space may be in a URL.
+        if ' #' in line:
+            line = line[:line.find(' #')]
+        # If there is a line continuation, drop it, and append the next line.
+        if line.endswith('\\'):
+            line = line[:-2].strip()
+            line += next(lines)
+        yield line.strip()
+
+
+def parse_requirements(strs):
+    for req in split_requirements(strs):
+        yield Requirement(req)
